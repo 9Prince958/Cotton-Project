a0fee980e7b06d7761341fa4012b17b05a5a0b5a
9Prince958 <filmzdroll@gmail.com>
2024-10-13 13:59:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+Shery.imageEffect("#rightimg img", {
+    style: 2, //Select Style
+    // debug: true, // Debug Panel
+    config: {
+        
+      /* Config made from debug panel */
+    },
+    preset: "./presets/wigglewobble.json",
+  });
+
+
